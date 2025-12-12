4ded070f0b9bcd3cfc3a6abc56ad860ee540a4a8
Nafi-GOAT <nafih2989@gmail.com>
2025-12-11 18:48:16 -0800

    Working on Milestone 1

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@
 #include <vector>
 #include <string>
 #include <cstdlib>
+#include <ctime>
 using namespace std;
 
 struct Customer {
@@ -21,3 +22,45 @@ struct Node {
 
     Node(const Customer& c) : data(c), next(nullptr) {} 
 };
+
+public:
+    Node* head;
+    Node* tail;
+
+    Queue() : head(nullptr), tail(nullptr) {}
+
+    ~Queue() {
+        while (!isEmpty()) {
+            dequeue();
+        }
+    }
+
+    bool dequeue(customer& out) {
+        if (isEmpty()) return false;
+
+        Node* temp = head;
+        out = head->data;
+        head = head->next;
+       
+        if (head == nullptr) 
+            tail = nullptr;
+            delete temp;
+        
+        return true;
+    }
+
+    void printQueue() const {
+        if (isEmpty()) {
+            cout << "The queue is empty." << endl;
+            return;
+        }
+
+        Node* current = head;
+        While (current != nullptr) {
+            cout << "Customer: " << current->data.name << ", Drink: " << current->data.drink << endl;
+            current = current->next;
+        }
+        cout << endl;
+    }
+};
+        
